a4330e66c6f0e2495edcbb15bb694d05e489c178
Math <mathilde.hetru@gmail.com>
2021-04-15 12:09:34 +0200

    calculette js

---
+++ JS-calculette.js
@@ -0,0 +1,27 @@
+// DOM Elements - touches calculette
+const ecranResults = document.querySelector(".results");
+const touchParaOuvrante = document.querySelector(".para-ouvrante");
+const touchParaFerme = document.querySelector(".para-ferme");
+const touchPourcentage = document.querySelector(".pourcentage");
+const touchCe = document.querySelector(".CE");
+const touchSept = document.querySelector(".sept");
+const touchHuit = document.querySelector(".huit");
+const touchNeuf = document.querySelector(".neuf");
+const touchDivision = document.querySelector(".division");
+const touchQuatre = document.querySelector(".quatre");
+const touchcinq = document.querySelector(".cinq");
+const touchSix = document.querySelector(".six");
+const touchMultiplie = document.querySelector(".multiplie");
+const touchUn = document.querySelector(".un");
+const touchDeux = document.querySelector(".deux");
+const touchTrois = document.querySelector(".trois");
+const touchMoins = document.querySelector(".moins");
+const touchZero = document.querySelector(".zero");
+const touchPoint = document.querySelector(".point");
+const touchEgal = document.querySelector(".egal");
+const touchPlus = document.querySelector(".plus");
+
+
+
+
+
