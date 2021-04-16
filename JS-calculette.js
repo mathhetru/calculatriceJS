ce42f4d3487c7a9b57b074149bcb7ad116a7d7e7
Mathilde Hetru <mathilde.hetru@gmail.com>
2021-04-16 16:45:30 +0200

    avancement JS

--- JS-calculette.js
+++ JS-calculette.js
@@ -9,7 +9,7 @@ const touchHuit = document.querySelector(".huit");
 const touchNeuf = document.querySelector(".neuf");
 const touchDivision = document.querySelector(".division");
 const touchQuatre = document.querySelector(".quatre");
-const touchcinq = document.querySelector(".cinq");
+const touchCinq = document.querySelector(".cinq");
 const touchSix = document.querySelector(".six");
 const touchMultiplie = document.querySelector(".multiplie");
 const touchUn = document.querySelector(".un");
@@ -23,25 +23,68 @@ const touchPlus = document.querySelector(".plus");
 
 
 // evenements click
-touchParaOuvrante.addEventListener('click', paraOuvrante);
-touchParaFerme.addEventListener('click', paraFerme);
-touchPourcentage.addEventListener('click', pourcentage);
-
-function paraOuvrante() {
-        ecranResults.innerHTML = "("
-};
-function paraFerme() {
+touchParaOuvrante.addEventListener('click', function paraOuvrante() {
+    ecranResults.innerHTML = "("
+});
+touchParaFerme.addEventListener('click', function paraFerme() {
     ecranResults.innerHTML = ")"
-};
-function pourcentage() {
+});
+touchPourcentage.addEventListener('click', function pourcentage() {
     ecranResults.innerHTML = "%"
-};
+});
+touchSept.addEventListener('click', function sept() {
+    ecranResults.innerHTML = "7"
+});
+touchHuit.addEventListener('click', function huit() {
+    ecranResults.innerHTML = "8"
+});
+touchNeuf.addEventListener('click', function neuf() {
+    ecranResults.innerHTML = "9"
+});
+touchDivision.addEventListener('click', function division() {
+    ecranResults.innerHTML = "/"
+});
+touchQuatre.addEventListener('click', function quatre() {
+    ecranResults.innerHTML = "4"
+});
+touchCinq.addEventListener('click', function cinq() {
+    ecranResults.innerHTML = "5"
+});
+touchSix.addEventListener('click', function six() {
+    ecranResults.innerHTML = "6"
+});
+touchMultiplie.addEventListener('click', function multiplie() {
+    ecranResults.innerHTML = "x"
+});
+touchUn.addEventListener('click', function un() {
+    ecranResults.innerHTML = "1"
+});
+touchDeux.addEventListener('click', function deux() {
+    ecranResults.innerHTML = "2"
+});
+touchTrois.addEventListener('click', function trois() {
+    ecranResults.innerHTML = "3"
+});
+touchMoins.addEventListener('click', function moins() {
+    ecranResults.innerHTML = "-"
+});
+touchZero.addEventListener('click', function zero() {
+    ecranResults.innerHTML = "0"
+});
+touchPoint.addEventListener('click', function point() {
+    ecranResults.innerHTML = "."
+});
+touchEgal.addEventListener('click', function egal() {
+    ecranResults.innerHTML = "="
+});
+touchPlus.addEventListener('click', function plus() {
+    ecranResults.innerHTML = "+"
+});
 
-
+/*
 
 // Evenements click
 
-/* 
 function showResults(message) {
 	ecranResults.innerHTML = (message);
 	};
